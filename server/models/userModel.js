444ef6bc6e2403cce51b80e92e5867ddfe268858
elcasado <139217641+elcasado@users.noreply.github.com>
2023-07-24 14:43:59 +0200

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema(
   {
-    userid: {
+    boaid: {
       type: String,
       required: [true, 'Please input email'],
     },
