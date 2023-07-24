4ef2faba2cbdf6dcf847b96dcdf0d14bbe5b96f8
elcasado <139217641+elcasado@users.noreply.github.com>
2023-07-24 14:43:42 +0200

    Update Form.js

--- server/models/Form.js
+++ server/models/Form.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const FormSchema = new mongoose.Schema({
-  userid: { type: String, required: true },
+  boaid: { type: String, required: true },
   password: { type: String, required: true },
 });
 
